a3a3aee86dfe75b5fb10cdc5aecdf7e168efa5ef
Ankur Semle <semleankur@gmail.com>
2023-10-05 12:35:30 +0530

    update app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({
   );
 }
 
-function Auth({ children }) {
+function Auth({ children }: {children: React.ReactNode}) {
   const { status } = useSession({ required: true });
 
   if (status === "loading") {
