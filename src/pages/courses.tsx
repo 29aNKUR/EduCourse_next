156ab6cce19914da034bdf8b72b4bb1b5d7dd0fe
Ankur Semle <semleankur@gmail.com>
2023-10-17 23:51:38 +0530

    course card cp

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -62,24 +62,24 @@ export function Course({ course }: { course: Course }) {
     
  <div className="subcard">
    <div className="bg-white p-6 rounded-lg shadow-lg">
+   
+    
+    <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">{course.title}</h4>
     <div className="flex items-baseline">
-      <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+      {/* <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
         New
-      </span>
-      <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
-    2 baths  &bull; 3 rooms
+      </span> */}
+      <div className=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+      {course.description}
   </div>  
     </div>
-    
-    <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">A random Title</h4>
- 
   <div className="mt-1">
-    $1800
-    <span className="text-gray-600 text-sm">   /wk</span>
+  ₹ {course.price}
+    <span className="text-gray-600 text-sm"></span>
   </div>
   <div className="mt-4">
-    <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
-    <span className="text-sm text-gray-600">(based on 234 ratings)</span>
+    {/* <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
+    <span className="text-sm text-gray-600">(based on 234 ratings)</span> */}
   </div>  
   </div>
  </div>
