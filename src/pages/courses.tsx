96292a06d9edab78ef795674e0a531effd315a35
Ankur Semle <semleankur@gmail.com>
2023-11-03 00:21:55 +0530

    change courses

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -32,64 +32,6 @@ function Courses() {
 
 export function Course({ course }: { course: Course }) {
   return (
-    // <div className= "shadow-lg dark:shadow-2xl p-5 w-52 lg:my-5 mt-10">
-    //   <Link href={`/courses/${course._id}`}>
-    //     <div className="mt-10 mx-10">
-    //       <img
-    //         className="p-8 rounded-lg4"
-    //         src={course.imageLink}
-    //         alt="product image"
-    //       />
-    //       <div className="px-5 pb-5 dark:bg-red-500">
-    //         <h5 className="text-lg font-bold tracking-wide">{course.title}</h5>
-    //         <h5 className="text-md font-semibold tracking-wide w-9 m-52">
-    //           {course.description}
-    //         </h5>
-    //         <div className="flex items-center justify-between">
-    //           <span className="text-lg font-bold">₹ {course.price}</span>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </Link>
-    // </div>
-
-    // <div className="card">
-    //   <Link href={`/courses/${course._id}`}>
-    //     <div className="wrapper bg-gray-400 antialiased text-gray-900 p-10">
-    //       <div>
-    //         <img
-    //           src={course.imageLink}
-    //           alt="product image"
-    //           className="w-full object-cover object-center rounded-lg shadow-md h-52"
-    //         />
-
-    //         <div className="subcard">
-    //           <div className="bg-white p-6 rounded-lg shadow-lg">
-    //             <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">
-    //               {course.title}
-    //             </h4>
-    //             <div className="flex items-baseline">
-    //               {/* <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
-    //     New
-    //   </span> */}
-    //               <div className=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
-    //                 {course.description}
-    //               </div>
-    //             </div>
-    //             <div className="mt-1">
-    //               ₹ {course.price}
-    //               <span className="text-gray-600 text-sm"></span>
-    //             </div>
-    //             <div className="mt-4">
-    //               {/* <span className="text-teal-600 text-md font-semibold">4/5 ratings </span>
-    // <span className="text-sm text-gray-600">(based on 234 ratings)</span> */}
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </Link>
-    // </div>
     <div className="hover:animate-bounce">
       <div className="card">
         <Link href={`/courses/${course._id}`}>
