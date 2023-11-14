113674840eaad2d25b0ad4ede4d40166985ecc4d
Ankur Semle <semleankur@gmail.com>
2023-11-14 13:07:03 +0530

    update shimmer

--- components/shimmer.tsx
+++ components/shimmer.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const shimmer = () => {
-  return (
-    <div className="course-card w-72 h-36 bg-white rounded-lg shadow-md overflow-hidden m-4 relative">
-      <div className="shimmer w-full h-full animate-shimmer"></div>
+const Shimmer = () => {
+  const shimmerCards = Array.from({ length: 10 }, (_, index) => (
+    <div key={index} className="course-card w-72 h-36 bg-white rounded-lg shadow-md overflow-hidden m-4 relative">
+      <div className="shimmer-container w-full h-full animate-shimmer">
+        <div className="shimmer-overlay"></div>
+      </div>
       <div className="course-info">
         <div className="shimmer w-3/4 h-4 mt-2 mb-2 animate-shimmer"></div>
         <div className="shimmer w-2/4 h-4 animate-shimmer"></div>
       </div>
     </div>
-  );
+  ));
+
+  return <div className="flex flex-wrap">{shimmerCards}</div>;
 };
 
-export default shimmer;
+export default Shimmer;
--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -3,12 +3,17 @@ import axios from "axios";
 import { Course } from "@/store/atoms/course.js";
 import Link from "next/link";
 import Shimmer from "../../components/shimmer";
+
 function Courses() {
   const [courses, setCourses] = useState([]);
 
   const allcourses = async () => {
-    const response = await axios.get(`/api/admin/courses/`);
-    setCourses(response.data.courses);
+    try {
+      const response = await axios.get(`/api/admin/courses/`);
+      setCourses(response.data.courses);
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
   };
 
   useEffect(() => {
@@ -17,12 +22,12 @@ function Courses() {
 
   return (
     <div>
-      {!courses ? (
-        <Shimmer />
+      {courses.length === 0 ? (
+        <Shimmer /> // Show shimmer while loading
       ) : (
         <div className="flex justify-center flex-wrap mt-10">
           {courses.map((course) => {
-            return <Course course={course} />;
+            return <Course key={course._id} course={course} />;
           })}
         </div>
       )}
@@ -48,8 +53,8 @@ export function Course({ course }: { course: Course }) {
               <div className="subcard" style={{ width: "100%" }}>
                 <div className="bg-white px-1 py-2 border md:px-4 md:py-1 rounded-lg shadow-lg flex flex-col justify-center">
                   <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate text-gray-600">
-                  <div style={{ maxWidth: "200px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
-                    {course.title}
+                    <div style={{ maxWidth: "200px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
+                      {course.title}
                     </div>
                   </h4>
                   <div className="text-gray-600 uppercase text-xs font-semibold tracking-wider overflow-hidden">
@@ -73,7 +78,8 @@ export function Course({ course }: { course: Course }) {
     </div>
   );
 }
+
 export default Courses;
 
-//added for custom client session handling
+// Added for custom client session handling
 Courses.auth = true;
