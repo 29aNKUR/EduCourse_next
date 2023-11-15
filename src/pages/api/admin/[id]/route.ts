6c6cd4a66abedd3b325c8d4a89925764807d75db
Ankur Semle <semleankur@gmail.com>
2023-11-15 23:46:37 +0530

    change id api route

--- src/pages/api/admin/[id]/route.ts
+++ src/pages/api/admin/[id]/route.ts
@@ -1,28 +1,29 @@
-
 import { Course } from "@/lib/db";
-import { ensureDbConnected } from "@/lib/dbConnect"
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { ensureDbConnected } from "@/lib/dbConnect";
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from "next-auth/react";
 
-export default async (req: NextApiRequest,res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = await getSession({ req });
 
-  const session =  await getSession({req});
-    if(!session) {
-      res.status(401).json({message: 'Unauthorized'});
-      return;
+    if (!session) {
+      return res.status(401).json({ message: 'Unauthorized' });
     }
 
     await ensureDbConnected();
     const courseId = req.query.id;
     console.log(courseId, "id");
-    const course = await Course.find({ _id: courseId});
-    if(course){
-      res.status(200).json({sucess: true , course});
+
+    const course = await Course.findById(courseId);
+
+    if (course) {
+      return res.status(200).json({ success: true, course });
+    } else {
+      return res.status(404).json({ success: false, message: "Course not found" });
     }
-    res.status(404).json({success: false, messgae: "Course not found"});
-
+  } catch (error) {
+    console.error("Error in API route:", error);
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-
-
-   
-    
+};
